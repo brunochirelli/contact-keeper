68b40ea43f6ea2b0c9d1115b06c0a46c537b4556
Bruno Chirelli <brunochirelli@gmail.com>
2020-07-08 07:58:17 -0300

    validate user registration inputs

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
 const router = express.Router();
+
+const { check, validationResult } = require("express-validator");
+
+const User = require("../models/User"); // Schema
 
 /**
  * A router object is an isolated instance of middleware and routes.
@@ -12,8 +17,61 @@ const router = express.Router();
  * @desc       Register a user
  * @access     Public
  */
-router.post("/", (req, res) => {
-    res.send("Register a user");
-});
+
+router.post(
+    "/",
+    [
+        check("name", "Please, add name").not().isEmpty(),
+        check("email", "Please, include a valid email")
+            .normalizeEmail()
+            .isEmail(),
+        check("password")
+            .not()
+            .isEmpty()
+            .withMessage("Please, insert a password")
+            .bail()
+            .isLength({
+                min: 6,
+            })
+            .withMessage("Please, insert a password with 6 or more characters"),
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        // validated
+        const { name, email, password } = req.body;
+
+        try {
+            let user = await User.findOne({ email });
+
+            if (user) res.status(400).json({ msg: "User already exists" });
+
+            // create a new instance for the user
+            user = new User({
+                name,
+                email,
+                password,
+            });
+
+            // generate a salt for hashing
+            const salt = await bcrypt.genSalt(10);
+
+            // assign a hash password for the input
+            user.password = await bcrypt.hash(password, salt);
+
+            // so, save the user
+            await user.save();
+
+            res.send("User saved");
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send("Server error"); // server error
+            // there is no need to send the error message for the user
+        }
+    }
+);
 
 module.exports = router;

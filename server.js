0a75c66cd2f2f472d6817d46750739116f064983
Bruno Chirelli <brunochirelli@gmail.com>
2020-08-05 10:51:46 -0300

    add static file serving

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const connectDB = require("./config/db");
 
@@ -13,12 +14,23 @@ app.use(express.json({ extended: false }));
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-    res.json({ msg: "Welcome to ContactKeeper API" });
+  res.json({ msg: "Welcome to ContactKeeper API" });
 });
 
 // Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Static Folder
+  app.use(express.static("client/build"));
+
+  // That are not any route defined...
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 app.listen(PORT, console.log(`server started on ${PORT}`));

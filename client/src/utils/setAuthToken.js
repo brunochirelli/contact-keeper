6711590c737c06c4d8973bb0068e920c3023a471
Bruno Chirelli <brunochirelli@gmail.com>
2020-08-05 10:52:47 -0300

    add backend auth

---
+++ client/src/utils/setAuthToken.js
@@ -0,0 +1,15 @@
+/**
+ * Set a default header with given token
+ */
+
+import axios from "axios";
+
+const setAuthToken = (token) => {
+  if (token) {
+    axios.defaults.headers.common["x-auth-token"] = token;
+  } else {
+    delete axios.defaults.headers.common["x-auth-token"];
+  }
+};
+
+export default setAuthToken;
--- middleware/auth.js
+++ middleware/auth.js
@@ -7,18 +7,23 @@ require("dotenv").config;
 const jwt = require("jsonwebtoken");
 
 module.exports = function (req, res, next) {
-    // has this token in the header?
-    const token = req.header("x-auth-token");
+  // check if this token are in the header
+  const token = req.header("x-auth-token");
 
-    if (!token) res.status(401).json({ msg: "No token, authorization denied" });
+  if (!token) {
+    res.status(401).json({ msg: "No token, authorization denied" });
+  }
 
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-        // we will decode the user in the payload and move on
-        req.user = decoded.user;
-        next();
-    } catch (err) {
-        res.status(401).json({ msg: "Token is not valid" });
+    // we will decode the user in the payload and move on
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    if (token) {
+      // and it's not valid
+      res.status(401).json({ msg: "Token is not valid" });
     }
+  }
 };
